deddd6d90e9c153cddfdaf02663460dc8652c683
eyalron33 <eyalron33@gmail.com>
2017-10-23 05:19:38 +0200

    init

---
+++ blockchainJS.js
@@ -0,0 +1,31 @@
+// BlockchainJS
+// Version 0.1
+//
+//  A collection of JS blockchain tools
+//
+
+function Blockchain() {
+  this.depth = 0; // example property of an object 
+}
+
+function GenerateRandomSeed() {
+
+  return seed;
+}
+
+function GenerateKeys(seed) 
+{
+
+  return {"sk" : sk,
+          "pk": pk};
+}
+
+function GenerateWallet(pk) {
+  return address;
+} 
+
+function GenerateHDWallet(pk, order) {
+  return address;
+}
+
+
